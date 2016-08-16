fe3df5888bb766ae5fa8c1b1fb8dfb6b363836b4
maxpleaner <maxpleaner>
2016-08-16 15:21:30 -0700

    add form_token helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def form_token
+    <<-HTML.strip_heredoc
+      <input type='hidden'
+             name='authenticity_token'
+             value='#{form_authenticity_token}'
+      ></input>
+    HTML
+  end
 end

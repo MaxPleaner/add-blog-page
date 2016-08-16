3da1cba1b9964868dcc150dced5853fc90603288
maxpleaner <maxpleaner>
2016-08-16 15:21:20 -0700

    add method to set instance variables for views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_instance_variables
+  def set_instance_variables
+    @sites = []
+  end
 end
